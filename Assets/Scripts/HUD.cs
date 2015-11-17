cb112c618666d0653c834a6ae2c65c49232086b7
Jefferson G Silva <jefferson7silva@gmail.com>
2015-11-17 01:43:52 -0200

    UI life
    array de lifes, script para controle do life. Script do player que tem
    as variaveis currentLife e maxLife

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour {
+
+    public Sprite[] LifeSprites;
+    public Image LifeUI;
+    private Player player;
+    //private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    void Update()
+    {
+         LifeUI.sprite = LifeSprites[player.CurrentLife];
+         Debug.Log(player.CurrentLife);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public int CurrentLife = 4;
+    public int MaxLife = 4;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
