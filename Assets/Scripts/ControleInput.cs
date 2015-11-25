eb07960eac7709fa8db047811e0854eb0a6f4b1c
cintra-bruno <bruno.cintra.tads@gmail.com>
2015-11-25 15:18:22 -0200

    Melhorias

--- Assets/Scripts/ControleInput.cs
+++ Assets/Scripts/ControleInput.cs
@@ -3,19 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class ControleInput : MonoBehaviour {
-
-	public Collider2D colliderEsquerdo;
-	public Collider2D colliderDireto;
+	
 	public float velocidade;
 	private GameObject player;
-	private enum direcao{DIREITA, ESQUERDA};
-	private direcao direcaoPlayer = direcao.DIREITA;
+	private bool isEsquerda;
+	private Rigidbody2D playerBody;
 
 
 	// Use this for initialization
 	void Start () {
 		//camera = GetComponent.<Camera>;
 		player = GameObject.Find("player");
+		// Recupera rigidbody do player
+		playerBody = player.GetComponent<Rigidbody2D> ();
 	}
 
 	
@@ -24,44 +24,51 @@ public class ControleInput : MonoBehaviour {
 		#if UNITY_EDITOR_WIN
 		// if unity editor
 		if (Input.GetMouseButtonDown(0))
-		{
-			Debug.Log("click");
-
+		{		
 			Vector3 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			Vector2 touchPos = new Vector2(wp.x, wp.y);
 
-			player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,300));
-
-			if(colliderEsquerdo.OverlapPoint(touchPos)) {
-				Debug.Log("apertou pra tras");
-
-				if(direcaoPlayer == direcao.DIREITA)
-				{
-					direcaoPlayer = direcao.ESQUERDA;
-					player.transform.eulerAngles = new Vector2(0,180);
-				}
+			if(touchPos.x > playerBody.position.x)
+			{
+				direta_touch();
 			}
-			if(colliderDireto.OverlapPoint(touchPos)) {
-				Debug.Log("apertou pra frente");
-
-				if(direcaoPlayer == direcao.ESQUERDA)
-				{
-					direcaoPlayer = direcao.DIREITA;
-					player.transform.eulerAngles = new Vector2(0,0);
-				}
+			else
+			{
+				esquerda_touch();
 			}
 		}
-
-		if(direcaoPlayer == direcao.DIREITA) {
-			transform.Translate (Vector2.right * velocidade * Time.deltaTime);
-		}
-		else
-		{
-			transform.Translate (-Vector2.right * velocidade * Time.deltaTime);
-		}
-
-		// endif unity editor
-		
 		#endif
+		player.transform.Translate (Vector2.right * velocidade * Time.deltaTime);
+
+	}
+
+	public void direta_touch()
+	{
+		Debug.Log("apertou pra frente");
+		voa ();
+		if(isEsquerda)
+		{
+			flipPersonagem();
+		}
+	}
+
+	public void esquerda_touch(){
+		Debug.Log("apertou pra tras");
+		voa ();
+		if(!isEsquerda)
+		{
+			flipPersonagem();
+		}
+	}
+
+	private void flipPersonagem()
+	{
+		isEsquerda = !isEsquerda;
+		player.transform.eulerAngles = new Vector2(0, isEsquerda ? 180 : 0);
+	}
+
+	private void voa()
+	{
+		playerBody.AddForce(new Vector2(0,300));
 	}
 }
