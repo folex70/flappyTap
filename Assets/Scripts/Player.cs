158f2ab9af75a9b30acb6916e53d9a3834a5bf73
cintra-bruno <bruno.cintra.tads@gmail.com>
2015-11-25 22:18:20 -0200

    melhorias

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,14 +5,48 @@ public class Player : MonoBehaviour {
 
     public int CurrentLife = 4;
     public int MaxLife = 4;
+	public bool dead = false;
+	public Animator anim;
 
     // Use this for initialization
     void Start () {
-	
+		GameObject view = this.transform.Find("view").gameObject ;
+		anim = view.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) 
+	{
+		if (coll.gameObject.tag == "walls") 
+		{
+			if(dead)
+			{
+				GameOver();
+			}
+			else
+			{
+				takeDamage(1);
+			}
+		}
+	}
+
+	public void takeDamage(int damage)	{
+		if (CurrentLife < 2) {
+			anim.Play("player_die");
+			CurrentLife = 0;
+			dead = true;
+		} else {
+			CurrentLife = CurrentLife - damage;
+			anim.Play("player_hit");
+		}
+	}
+
+	void GameOver(){
+		Application.LoadLevel (Application.loadedLevel);
 	}
 }
