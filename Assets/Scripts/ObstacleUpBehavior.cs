69301cd1fdf8701b756847d1263e7f82171ccd0c
Jefferson G Silva <jefferson7silva@gmail.com>
2015-11-27 01:08:09 -0200

    Obstaculos
    Obstaculos aleatorios; ObjectPool; Tags e Spawners

---
+++ Assets/Scripts/ObstacleUpBehavior.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//usar gameObject aqui faz referencia a esse game object, no caso, o obstaculo que esse script vai ser associado
+public class ObstacleUpBehavior : MonoBehaviour {
+	
+	public Vector2 velocity = new Vector2(-5,0);
+	public float range = 10;
+	public float valorY = 0f;
+	public float valorXTeto = 0f;
+	public float valorYTeto = 0f;
+	
+	
+	void OnEnable()
+	{
+		GetComponent<Rigidbody2D>().velocity =velocity;
+		valorXTeto = GameObject.FindWithTag("spawn_up").transform.position.x;
+		valorYTeto = GameObject.FindWithTag("spawn_up").transform.position.y;
+		valorY = valorYTeto - range * Random.Range (0.1f, 0.4f);
+		
+		
+		transform.position = new Vector3 (valorXTeto,
+		                                  valorY,
+		                                  transform.position.z);
+		
+		Invoke ("Destroy", 7f);
+		
+	}
+	
+	void Destroy()
+	{
+		gameObject.SetActive (false);
+	
+	}
+	
+	void OnDisable()
+	{
+		CancelInvoke ();
+	}
+	
+}
